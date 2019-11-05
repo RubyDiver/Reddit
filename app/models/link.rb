13be7dc72dba04234c22ea587b44410b8f041c60
Paweł <tatarynowiczpawel2@gmail.com>
2019-11-05 23:45:54 +0100

    Authorization on links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,6 @@
 class Link < ApplicationRecord
+
+  validates :title, presence: true, length: {minimum: 3, maximum: 30}
+  validates :url, presence:  true, length:  {minimum: 3, maximum: 50}
   belongs_to :user
 end
