c60f66982f9725fc065ff21dcdc025f56c346dca
Paweł <tatarynowiczpawel2@gmail.com>
2019-11-08 23:04:17 +0100

    Add name to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  # acts_as_votable
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
+
   def create
     @link = Link.find(params[:link_id])
     @comment = @link.comments.new(comment_params)
